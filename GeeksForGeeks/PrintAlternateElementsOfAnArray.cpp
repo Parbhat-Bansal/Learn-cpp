be12c5d5dc1cc3319146d0506affc8f8f62f858d
Parbhat Bansal <parbhat.bansal001@gmail.com>
2023-12-27 19:39:24 +0530

    Print alternate element is an array

---
+++ GeeksForGeeks/PrintAlternateElementsOfAnArray.cpp
@@ -0,0 +1,11 @@
+void print(int ar[], int n)
+{
+    for(int i = 0 ; i<n ; i=i+2)
+    {
+        cout << ar[i] << " "  ;
+    }
+    
+    
+}
+// que link --->
+// https://www.geeksforgeeks.org/problems/print-alternate-elements-of-an-array/1?page=1&category=Arrays&difficulty=School&sortBy=submissions
