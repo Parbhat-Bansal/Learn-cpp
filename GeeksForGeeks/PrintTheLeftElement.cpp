5a293c3e58b7a6829dda489734457e02acdbcec1
Parbhat Bansal <parbhat.bansal001@gmail.com>
2023-12-27 13:30:44 +0530

    Adding code

---
+++ GeeksForGeeks/PrintTheLeftElement.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+        int leftElement(int a[], int n) {
+            // Your code goes here 
+        sort(a, a + n);
+        if (n % 2 == 0) {
+            return a[(n / 2) - 1];
+        } else {
+            return a[n / 2];
+        }
+    }
+};
+
+//Question link 
+// https://www.geeksforgeeks.org/problems/print-the-left-element2009/1?page=1&category=Arrays&difficulty=School&sortBy=submissions
